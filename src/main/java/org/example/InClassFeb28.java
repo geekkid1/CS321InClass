7c6a136dcfd922440d63223d868744429234a129
geekkid1 <captain.emily.blum@gmail.com>
2024-02-28 19:06:22 -0500

    Initial Commit

---
+++ src/main/java/org/example/InClassFeb28.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class InClassFeb28 {
+    private int wfid;
+    private String step;
+    private int objid;
+
+    public boolean addWFItem(String step, int objid) {
+        return false;
+    }
+
+    public int getNextWFItem(String step) {
+        return 0;
+    }
+
+    public int countWFItems(String step) {
+        return 0;
+    }
+}
